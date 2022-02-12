93dca42f3c7b99d1ef1851d66b3f14772dd3ab83
HectorBusSan <99218752+HectorBusSan@users.noreply.github.com>
2022-02-11 22:35:34 -0600

    Create ejercicio1.py

---
+++ Modulo 6/ejercicio1.py
@@ -0,0 +1,6 @@
+planets=['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+for item in planets:
+    print(item)
+planets.append('Pluton')
+print(len(planets))
+print(planets)
