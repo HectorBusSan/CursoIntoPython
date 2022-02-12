e612c7ebeabf8d2471251bcaa7b6cfebf43f317d
HectorBusSan <99218752+HectorBusSan@users.noreply.github.com>
2022-02-11 22:34:43 -0600

    Create ejercicio1.py

---
+++ Modulo 5/ejercicio1.py
@@ -0,0 +1,10 @@
+distancia1= 149597870
+distancia2= 778547200
+
+kilometros= distancia1 - distancia2
+millas= kilometros * .621
+print(distancia1*.621,' km')
+print(distancia2*.621,' km')
+print('\nDistancia entre planetas')
+print(round(abs(millas)),'millas')
+print(abs(kilometros),'km')
